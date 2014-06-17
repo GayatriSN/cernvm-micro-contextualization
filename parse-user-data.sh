5b938390362e52c39e852c8ebd7d4e9f8a125def
GayatriSN <sacredlight2709@gmail.com>
2014-06-18 03:28:09 +0530

    Added utility for startup-script

--- parse-user-data.sh
+++ parse-user-data.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-USER_DATA=~/ud
-
 grep "MIME" ${USER_DATA} | cat > mimetest.txt
 
 #We create temporary copy of user-data file
@@ -24,11 +22,21 @@ else
 
 	#Generating a cloud-init compatible MIME-Multipart
 
-	#Since the boundary value and the header is of no interest to us or cloud-init,
-	#we statically generate this to avoid any additional dependencies or use of python
+	#Since the source our mime file will always be cernvm the boundary value and the header
+	#in mime could be statically generated to avoid any additional dependencies or use of python
 	#These static files are stored in the mime-files folder
 
 	cp mime-files/main-mime-header mime-user-data
+
+	#STARTUP SCRIPT SECTION
+	sed -n '/^#!\/bin\/sh$/,/^\[ucernvm-begin\]\|\[amiconfig\]\|#cloud-config$/p' ud | head -n -1 >> /tmp/startup-script
+	if [ -s /tmp/startup-script ]
+	then
+		echo "--===============1341993424==" >> mime-user-data
+                cat mime-files/startup-script-mime-header >> mime-user-data
+                cat /tmp/startup-script >> mime-user-data
+	fi
+	rm /tmp/startup-script
 
 	#UCERNVM SECTION
 	if [ -s /tmp/ucernvm-data ]
