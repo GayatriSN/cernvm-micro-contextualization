c6eaff83061394e3a4dffcdbfebe1d7b71b2856a
GayatriSN <sacredlight2709@gmail.com>
2014-06-23 11:38:15 +0530

    Merged uc-user-data.sh functionality in get-user-data.sh

--- get-user-data.sh
+++ get-user-data.sh
@@ -23,6 +23,17 @@ for PROVIDER in provider.d/*; do
    fi
 done
 
+file /tmp/user-data* | grep "compressed" | cat > com.txt
+
+if [ -s com.txt ]
+then
+        gzip -d /tmp/user-data* 
+else
+        echo "File not compressed"
+fi
+
+rm com.txt
+
 cat $USER_DATA
 
 rm dmesg.txt
