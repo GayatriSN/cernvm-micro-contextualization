2c9587edee3a8ae2ffdb45c269da7d133db86d55
GayatriSN <sacredlight2709@gmail.com>
2014-06-24 02:33:14 +0530

    corrected indentation issue

--- system-boot/cloudinit/handlers/amiconfig.py
+++ system-boot/cloudinit/handlers/amiconfig.py
@@ -25,20 +25,20 @@ class AmiconfigPartHandler(handlers.Handler):
     def handle_part(self, _data, ctype, filename,  # pylint: disable=W0221
                     payload, frequency):  # pylint: disable=W0613
 
-    #This part-handler functions as a wrapper script that will set the
-    #environment variables and copy files for amiconfig and then call it    
+        #This part-handler functions as a wrapper script that will set the
+        #environment variables and copy files for amiconfig and then call it    
 
         if ctype in handlers.CONTENT_SIGNALS:
             #Do Nothing at beginning or end of our handler [to be added if needed]
             return
 
-    #Copying amiconfig relevant part to a new file /var/lib/cloud/amiconfig-data
-    util.write_file(amiconfig_file, payload, 0777)
+        #Copying amiconfig relevant part to a new file /var/lib/cloud/amiconfig-data
+        util.write_file(amiconfig_file, payload, 0777)
 
-    #Setting the environment for amiconfig to user the local user-data  
-    os.environ["AMICONFIG_LOCAL_USER_DATA"] = "/var/lib/cloud/amiconfig-data"
+        #Setting the environment for amiconfig to user the local user-data  
+        os.environ["AMICONFIG_LOCAL_USER_DATA"] = "/var/lib/cloud/amiconfig-data"
 
-    #Executing amiconfig
-    print "Running amiconfig contextualization"
-    os.system("/usr/bin/amiconfig")
+        #Executing amiconfig
+        print "Running amiconfig contextualization"
+        os.system("/usr/bin/amiconfig")
 
