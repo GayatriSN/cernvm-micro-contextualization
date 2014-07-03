d3dad2b49c90cb88dae9204212ad38a9e042c0d6
GayatriSN <sacredlight2709@gmail.com>
2014-07-03 15:53:16 +0530

    Added temporary meta-data seeding for Cloud-Init

--- system-boot/seed-user-data.sh
+++ system-boot/seed-user-data.sh
@@ -7,11 +7,11 @@ cat supported-providers-list | grep $PROVIDER | cat > provider-check.txt
 
 if [ -s provider-check.txt ]
 then
-	echo "Provider supported by cloud-init"
+	echo "Provider supported by both cloud-init and micro-bootloader"
 else
 	PROVIDER="nocloud-net"
-	echo "Cloud Provider not supported by CloudInit"
-	echo "Setting provider to nocloud-net"
+	echo "Cloud Provider not supported by both"
+	echo "Setting provider to nocloud-net..."
 fi
 
 rm provider-check.txt
@@ -23,3 +23,7 @@ mkdir -p $SEED_DIR
 cp /var/lib/micro-user-data ${SEED_DIR}/user-data
 
 cat ${SEED_DIR}/user-data
+
+#Creating a dummy meta-data file until a proper meta-data fetching utility is implemented
+
+echo "Sample meta-data" | cat > ${SEED_DIR}/meta-data
