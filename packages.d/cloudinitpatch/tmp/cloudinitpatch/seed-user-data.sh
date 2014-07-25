6af0c977c141ddc7877954888d5d4fc0acb33c60
GayatriSN <sacredlight2709@gmail.com>
2014-07-25 13:31:14 +0530

    Minor fix

--- packages.d/cloudinitpatch/tmp/cloudinitpatch/seed-user-data.sh
+++ packages.d/cloudinitpatch/tmp/cloudinitpatch/seed-user-data.sh
@@ -3,7 +3,7 @@
 providerfile="/var/lib/cloud-provider"
 PROVIDER=$(cat $providerfile)
 
-cat supported-providers-list | grep $PROVIDER | cat > provider-check.txt
+cat supported-providers-list | grep ${PROVIDER} | cat > provider-check.txt
 
 if [ -s provider-check.txt ]
 then
@@ -16,9 +16,9 @@ fi
 
 rm provider-check.txt
 
-SEED_DIR="/var/lib/cloud/seed/$PROVIDER"
+SEED_DIR="/var/lib/cloud/seed/${PROVIDER}"
 
-mkdir -p $SEED_DIR 
+mkdir -p ${SEED_DIR} 
 
 cp /var/lib/micro-user-data ${SEED_DIR}/user-data
 
