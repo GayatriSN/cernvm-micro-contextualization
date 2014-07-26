7c84d9cb1c71f8ab96d646c34d9151eb6f22a2e1
GayatriSN <sacredlight2709@gmail.com>
2014-07-26 14:33:29 +0530

    Modified to work with chkconfig

--- packages.d/cloudinitpatch/tmp/cloudinitpatch/seed-user-data.sh
+++ packages.d/cloudinitpatch/tmp/cloudinitpatch/seed-user-data.sh
@@ -1,29 +1,59 @@
 #!/bin/sh
+#
+# chkconfig: 345 20 20
+# description: Seed User Data obtained in micro boot stage.
 
-providerfile="/var/lib/cloud-provider"
-PROVIDER=$(cat $providerfile)
+RETVAL=0
 
-cat supported-providers-list | grep ${PROVIDER} | cat > provider-check.txt
+start() {
+	echo "Seeding User Data and Meta Data..." 
 
-if [ -s provider-check.txt ]
-then
-	echo "Provider supported by both cloud-init and micro-bootloader"
-else
-	PROVIDER="nocloud-net"
-	echo "Cloud Provider not supported by both"
-	echo "Setting provider to nocloud-net..."
-fi
+	providerfile="/var/lib/micro-cloud-provider"
 
-rm provider-check.txt
+	if [ ! -s ${providerfile} ]
+	then
+		echo "nocloud-net" | cat > ${providerfile}
+	fi
 
-SEED_DIR="/var/lib/cloud/seed/${PROVIDER}"
+	PROVIDER=$(cat $providerfile)
 
-mkdir -p ${SEED_DIR} 
+	cat /var/lib/supported-providers-list | grep ${PROVIDER} | cat > provider-check.txt
 
-cp /var/lib/micro-user-data ${SEED_DIR}/user-data
+	if [ -s provider-check.txt ]
+	then
+		echo "Provider supported by both cloud-init and micro-bootloader"
+	else
+		PROVIDER="nocloud-net"
+		echo "Cloud Provider not supported by both"
+		echo "Setting provider to nocloud-net..."
+	fi
+	
+	rm provider-check.txt
 
-cat ${SEED_DIR}/user-data
+	SEED_DIR="/var/lib/cloud/seed/${PROVIDER}"
 
-#Creating a dummy meta-data file until a proper meta-data fetching utility is implemented
+	mkdir -p ${SEED_DIR} 
 
-echo "Sample meta-data" | cat > ${SEED_DIR}/meta-data
+	cp /var/lib/micro-user-data ${SEED_DIR}/user-data
+
+	cat ${SEED_DIR}/user-data
+
+	#Creating a dummy meta-data file until a proper meta-data fetching utility is implemented
+
+	echo "Sample meta-data" | cat > ${SEED_DIR}/meta-data
+
+	return 0
+}
+
+stop() {
+	echo "Terminating Seed Script..."
+	return 0
+}
+
+case "$1" in
+	start) start; RETVAL=$? ;;
+	stop) stop; RETVAL=$? ;;
+	*) RETVAL=1 ;;
+esac
+
+exit $RETVAL
