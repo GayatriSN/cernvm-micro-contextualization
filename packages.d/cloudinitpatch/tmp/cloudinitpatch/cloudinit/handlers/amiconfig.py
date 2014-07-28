a1c7a898391947de8898f4dce19932843b358b4d
GayatriSN <sacredlight2709@gmail.com>
2014-07-28 10:28:35 +0530

    Modified amiconfig Part-Handler structure

--- packages.d/cloudinitpatch/tmp/cloudinitpatch/cloudinit/handlers/amiconfig.py
+++ packages.d/cloudinitpatch/tmp/cloudinitpatch/cloudinit/handlers/amiconfig.py
@@ -1,44 +1,24 @@
-#custom part handler for amiconfig
+#part-handler
+# vi: syntax=python ts=4
 
-import os
+def list_types():
+	return [("text/amiconfig")]
 
-from cloudinit import handlers
-from cloudinit import log as logging
-from cloudinit import util
+def handle_part(data,ctype,filename,payload):
+	if ctype == "__begin__":
+		print "Amiconfig Part Handler is beginning"
+		return
+	if ctype == "__end__":
+		print "Amiconfig Part Handler is beginning"
+		return
 
-from cloudinit.settings import (PER_ONCE)
-
-LOG = logging.getLogger(__name__)
-AMICONFIG_PREFIX = "#amiconfig"
-
-
-class AmiconfigPartHandler(handlers.Handler):
-    def __init__(self, paths, **_kwargs):
-        handlers.Handler.__init__(self, PER_ONCE)
-        self.amiconfig_file = "/var/lib/cloud/amiconfig-data"
-
-    def list_types(self):
-        return [
-            handlers.type_from_starts_with(AMICONFIG_PREFIX),
-        ]
-
-    def handle_part(self, _data, ctype, filename,  # pylint: disable=W0221
-                    payload, frequency):  # pylint: disable=W0613
-
-        #This part-handler functions as a wrapper script that will set the
-        #environment variables and copy files for amiconfig and then call it    
-
-        if ctype in handlers.CONTENT_SIGNALS:
-            #Do Nothing at beginning or end of our handler [to be added if needed]
-            return
-
-        #Copying amiconfig relevant part to a new file /var/lib/cloud/amiconfig-data
-        util.write_file(amiconfig_file, payload, 0777)
-
-        #Setting the environment for amiconfig to user the local user-data  
-        os.environ["AMICONFIG_LOCAL_USER_DATA"] = "/var/lib/cloud/amiconfig-data"
-
-        #Executing amiconfig
-        print "Running amiconfig contextualization"
-        os.system("/usr/bin/amiconfig")
-
+	print "Received Content Type=%s Filename=%s" % (ctype,filename)
+	import os
+	f = open('/var/lib/cloud/amiconfig-data','w')
+	f.write(payload)
+	f.close()
+	os.chmod('var/lib/cloud/amiconfig-data','0777')
+	os.environ["AMICONFIG_LOCAL_USER_DATA"] = '/var/lib/cloud/amiconfig-data'
+	print "Running amiconfig contextualization"
+	os.system('/usr/bin/amiconfig')
+	print "Ending Amiconfig"
