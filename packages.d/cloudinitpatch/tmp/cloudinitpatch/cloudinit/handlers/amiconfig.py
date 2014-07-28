b8f85135db0eedfe8411d747f99ed8cdd078230a
GayatriSN <sacredlight2709@gmail.com>
2014-07-29 00:06:56 +0530

    Fixed amiconfig path

--- packages.d/cloudinitpatch/tmp/cloudinitpatch/cloudinit/handlers/amiconfig.py
+++ packages.d/cloudinitpatch/tmp/cloudinitpatch/cloudinit/handlers/amiconfig.py
@@ -14,11 +14,11 @@ def handle_part(data,ctype,filename,payload):
 
 	print "Received Content Type=%s Filename=%s" % (ctype,filename)
 	import os
-	f = open('/var/lib/cloud/amiconfig-data','w')
-	f.write(payload)
-	f.close()
-	os.chmod('var/lib/cloud/amiconfig-data','0777')
+	with open('/var/lib/cloud/amiconfig-data','w') as fnew:
+		fnew.write(payload)
+		fnew.close()
+	os.chmod('var/lib/cloud/amiconfig-data',0777)
 	os.environ["AMICONFIG_LOCAL_USER_DATA"] = '/var/lib/cloud/amiconfig-data'
-	print "Running amiconfig contextualization"
-	os.system('/usr/bin/amiconfig')
+	print "Running amiconfig contextualization..."
+	os.system('/usr/sbin/amiconfig')
 	print "Ending Amiconfig"
